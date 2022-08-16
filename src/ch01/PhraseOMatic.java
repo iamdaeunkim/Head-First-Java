11be219f008306ef0654f5ad0d555ee90ec601b4
idaeunkim <kdaeun1025@gmail.com>
2022-08-17 00:08:51 +0900

    ch01

---
+++ src/ch01/PhraseOMatic.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class PhraseOMatic {
+    public static void main (String[] args) {
+
+        //세 종류의 단어 목록 생성
+        String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma", "critical-path", "dynamic"};
+
+        String[] wordListTwo = {};
+    }
+}
